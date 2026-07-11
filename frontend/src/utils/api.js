08d07f171843c85b8152df2cb82b644b223f86f2
kayy0y <kaushikikamna012@gmail.com>
2026-07-11 20:39:26 +0530

    api modified

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://siem-system.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
